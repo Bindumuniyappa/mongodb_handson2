4e17564d9e41d9ee97c8a24178e2e25ddf2c5904
Bindumuniyappa <muniyappabindu7@gmail.com>
2023-03-24 14:34:12 +0530

    handson 2

---
+++ database/connection.js
@@ -0,0 +1,46 @@
+const {MongoClient} = require("mongodb")
+
+const url="mongodb+srv://muniyappabindu7:Bindu123@cluster0.otsvnjq.mongodb.net/?retryWrites=true&w=majority";
+const client=new MongoClient(url);
+
+const insertToDB=async (data) =>{
+    await client.connect();
+    const database=client.db("student")
+    const collection=database.collection("information")
+    await client.connect();
+    const dbResponse=await collection.insertOne(data);
+    await client.close();
+    return dbResponse;
+}
+
+
+const findInDB=async (query)=>{
+    const database=client.db("student");
+    const collection=database.collection("information");
+    await client.connect();
+    const dbResponse=await collection.find(query).toArray();
+    await client.close();
+    return dbResponse;
+}
+
+const updateInDB = async (filteredCond, value) => {
+    const database = client.db("student");
+    const collection = database.collection("information");
+    await client.connect();
+    const dbResponse = await collection.updateOne(filteredCond, value);
+    await client.close();
+    return dbResponse; 
+}
+
+const deleteInDB = async (filteredCond) => {
+    const database = client.db("student");
+    const collection = database.collection("information");
+    await client.connect();
+    const dbResponse = await collection.deleteOne(filteredCond);
+    await client.close();
+    return dbResponse; 
+}
+
+module.exports={
+    insertToDB,findInDB,updateInDB,deleteInDB,
+}
